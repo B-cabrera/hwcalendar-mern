d2b314bf13bc756fdfe88a01003d0e7d99b49825
B-cabrera <brendenc29@gmail.com>
2023-01-23 10:48:57 -0500

    setup basic page layout, added button to add and enter class name.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,52 @@
+import { ChangeEvent, useState } from 'react'
 import './App.css'
+import NavBar from './components/NavBar';
+
+
 
 function App() {
+  const [isAdding, setIsAdding] = useState(false);
+  const [className, setClassName] = useState('');
+
+  function changeAdd() {
+    if (!isAdding) setIsAdding(true);
+  }
+
 
   return (
-    <p>Hello World</p>
+    <div className='App'>
+      <NavBar />
+      <div id='content'>
+        <div id="top">
+          {!isAdding &&
+          <button
+            id='classbtn'
+            onClick={changeAdd}>
+            Add Class
+          </button>}
+        </div>
+        <div id='rest'>
+          <div id='middle'>
+            {className}
+          </div>
+
+          <div id="bottom">
+            {isAdding &&
+              <form>
+                <label htmlFor="classinput">Enter the Class Name</label>
+                <input 
+                  id="classinput"
+                  onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                    event.preventDefault();
+                    setClassName(event.target.value)  
+                  }} />
+                <button id="submitbtn">Enter</button>
+              </form>
+            }
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+
+
+export default function NavBar() {
+    return (
+        <div id="navbar">
+            <div>
+                <h1 id="logo">[Add Logo Here]</h1>
+            </div>
+            <div>
+                <h1>HwCalendar</h1>
+            </div>
+        </div>
+    );
+}
