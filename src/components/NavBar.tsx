036b5dbc8bd35158ff5ec638e00d2b4f64106261
B-cabrera <brendenc29@gmail.com>
2023-02-02 21:34:31 -0500

    added logo to navbar and made it a link to the home pag e

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,14 +2,21 @@ import { Link } from "react-router-dom"
 import "../styles/NavBar.css"
 
 export default function NavBar() {
-    return (
-        <div id="navbar">
-            <div>
-                <h1 id="logo">[Add Logo Here]</h1>
-            </div>
-            <div>
-                <h1><Link to='/'>HwCalendar</Link></h1>
-            </div>
-        </div>
-    );
+  return (
+    <div id="navbar">
+      <div>
+        <Link to='/'>
+          <img
+            id="logo"
+            src="/src/assets/HWCalendarLogo.png"
+          />
+        </Link>
+      </div>
+      <div>
+        <h1><Link to='/'>
+          HwCalendar
+        </Link></h1>
+      </div>
+    </div>
+  );
 }
