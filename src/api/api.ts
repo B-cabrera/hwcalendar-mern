919e0fd3a2f9795dd1d568037ea07d134e73d4f8
B-cabrera <brendenc29@gmail.com>
2023-05-21 23:33:25 -0400

    updated comments on api file

--- src/api/api.ts
+++ src/api/api.ts
@@ -11,17 +11,19 @@ import { verifyToken } from './middleware';
 
 const router = Router();
 
-router.get('/', verifyToken, getAllClassNames)
-router.get('/:id', verifyToken, getAssignmentsByClassID)
-router.get('/auth/googleClient', getGoogleClientID);
-router.post('/class', verifyToken, createNewClass)
-router.post('/assignment', verifyToken, createNewAssignment)
-router.post('/auth', initAuth);
+// ROUTES WITH COMMAS MEAN I HAVE LOOKED OVER IT AND FIXED IT AFTER THE ADDITION OF USER AUTH
+
+router.get('/', verifyToken, getAllClassNames);// DONE
+router.get('/:id', verifyToken, getAssignmentsByClassID);
+router.get('/auth/googleClient', getGoogleClientID); // DONE
+router.post('/class', verifyToken, createNewClass); // DONE
+router.post('/assignment', verifyToken, createNewAssignment);
+router.post('/auth', initAuth); // DONE
 router.patch('/assignment', verifyToken, toggleAssignment);
 router.patch('/assignment/:id', verifyToken, updateAssignment);
-router.patch('/class/:id', verifyToken, updateClassName)
+router.patch('/class/:id', verifyToken, updateClassName);
 router.delete('/class/:id', verifyToken, deleteClass);
-router.delete('/assignment/:hwID/:classID', verifyToken, deleteAssignment)
+router.delete('/assignment/:hwID/:classID', verifyToken, deleteAssignment);
 
 
 
