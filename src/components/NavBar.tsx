596733f39b08a4666b2082e40935cfd182c9958e
B-cabrera <brendenc29@gmail.com>
2023-05-25 00:48:42 -0400

    added logout button and made changes to navbar for mobile

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,22 +1,32 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import "../styles/NavBar.css"
 
 export default function NavBar() {
+  const navigate = useNavigate();
+
+  function logUserOut() {
+    sessionStorage.removeItem('token');
+    navigate('/login');
+  }
+
+
   return (
     <div id="navbar">
-      <div>
+      <div id="name">
         <Link to='/'>
           <img
             id="logo"
             src="/src/assets/HWCalendarLogo.png"
           />
         </Link>
-      </div>
-      <div>
         <h1><Link to='/'>
           HwCalendar
         </Link></h1>
       </div>
+        <button 
+        id="logoutbtn"
+        onClick={logUserOut}
+        >Logout</button>
     </div>
   );
 }
