ad3e3fcf39dd9acb74c855e4d483e75b7553acfc
B-cabrera <brendenc29@gmail.com>
2023-06-29 20:10:39 -0700

    updated rest of images to show on deployed website

--- frontend/src/components/LandingPage.tsx
+++ frontend/src/components/LandingPage.tsx
@@ -44,7 +44,7 @@ export default function LandingPage() {
       <span id='heading'>
         <img
           className='logo'
-          src='/src/assets/HwCalendarLogo.png'
+          src='/HwCalendarLogo.png'
         />
         <h1>HwCalendar</h1>
       </span>
@@ -54,14 +54,14 @@ export default function LandingPage() {
       </span>
       <div id='description'>
         <p><u>You can create a class with a collection of homework's by just pressing 'Add Class'!</u></p>
-        <img className='exampleimg' src="/src/assets/add-class.png" />
+        <img className='exampleimg' src="/add-class.png" />
         <p><u>When you are inside the class page, you can add homework's with the date that it is due.</u></p>
-        <img className='exampleimg' src="/src/assets/add-hw.png" />
+        <img className='exampleimg' src="/add-hw.png" />
         <p><u>Homeworks are now stored and can be toggled finished or not finished by just pressing the checkbox!</u></p>
-        <img className='exampleimg' src="/src/assets/toggle-finish.png" />
+        <img className='exampleimg' src="/toggle-finish.png" />
         <p><u>Homeworks are sorted by unfinished first, and finished last to ensure your most
           important ones are always at the top!</u></p>
-        <img className='exampleimg' src="/src/assets/sorted-hw.png" />
+        <img className='exampleimg' src="/sorted-hw.png" />
       </div>
 
     </>
